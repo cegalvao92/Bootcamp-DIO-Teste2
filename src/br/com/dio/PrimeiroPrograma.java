bdd0e8066b2a68d954cdac7fd523fe1edc25e8a3
Eduardo <cegalvao13@gmail.com>
2022-05-19 11:12:54 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+    }
+}
